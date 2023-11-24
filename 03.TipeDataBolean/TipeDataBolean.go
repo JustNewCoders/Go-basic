79d5642a08e3b07a135771cfd2bf41e4e6ec364b
JustNewCoders <kyuubrawlhalla@gmail.com>
2023-11-25 01:45:59 +0700

    03.TipeDataBolean

---
+++ 03.TipeDataBolean/TipeDataBolean.go
@@ -0,0 +1,16 @@
+/*
+todo: start
+!<==== Tipe data Boolean ====>
+* memiliki dua nilai yaitu, true & false
+* kata kunci bool
+todo: end
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("benar =", true)
+	fmt.Println("salah =", false)
+}
