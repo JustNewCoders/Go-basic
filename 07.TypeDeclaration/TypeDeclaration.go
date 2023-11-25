1005ebe92ccf6488557559e5e4421736f5792de9
JustNewCoders <kyuubrawlhalla@gmail.com>
2023-11-25 08:05:46 +0700

    07.TypeDeclaration

---
+++ 07.TypeDeclaration/TypeDeclaration.go
@@ -0,0 +1,23 @@
+/*
+todo: start
+!<==== Type Declaration ====>
+* Type Declarations adalah kemampuan membuat ulang tipe data baru dari tipe data yang sudah ada
+* Type Declarations biasanya digunakan untuk membuat alias terhadap tipe data yang sudah ada, dengan tujuan agar lebih mudah dimengerti
+
+todo: end
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	type Names string
+	var firstname Names = "Wiliam"
+	var midlename Names = "Fery"
+	var lastname Names = "Sagala"
+
+	fmt.Println("Name:", firstname,midlename,lastname)
+	
+}
