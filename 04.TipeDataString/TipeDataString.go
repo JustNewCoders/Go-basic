b4c30721e0092ad92b00f56810dbc59cfd22ae29
JustNewCoders <kyuubrawlhalla@gmail.com>
2023-11-25 07:39:19 +0700

    04.TipeDataString

---
+++ 04.TipeDataString/TipeDataString.go
@@ -0,0 +1,28 @@
+/*
+todo: start
+!<==== Tipe Data String ====>
+* String ada tipe data kumpulan karakter
+* String ada tipe data kumpulan karakter
+* Jumlah karakter di dalam String bisa nol sampai tidak terhingga
+* Tipe data String di Go-Lang direpresentasikan dengan kata kunci string
+* Nilai data String di Go-Lang selalu diawali dengan karakter “ (petik dua) dan diakhiri dengan karakter “ (petik dua)
+
+? Function untuk String
+_______________________________________________________________________
+|	function					|	keterangan																			|
+-----------------------------------------------------------------------
+|	len("string")			|	Menghitung jumlah karakter di String						|
+|	"String"[number] 	|	Mengambil karakter pada posisi yang ditentukan	|
+-----------------------------------------------------------------------
+todo: end
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Wiliam")
+	fmt.Println("Wiliam Fery")
+	fmt.Println("Wiliam Fery Sagala")
+	
+}
