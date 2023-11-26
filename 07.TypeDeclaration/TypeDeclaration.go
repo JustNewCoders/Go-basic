ac38ae1ca37e888deeacd70c56749d530d13b30c
JustNewCoders <kyuubrawlhalla@gmail.com>
2023-11-26 07:29:19 +0700

    07.TypeDeclaration

--- 07.TypeDeclaration/TypeDeclaration.go
+++ 07.TypeDeclaration/TypeDeclaration.go
@@ -3,7 +3,6 @@ todo: start
 !<==== Type Declaration ====>
 * Type Declarations adalah kemampuan membuat ulang tipe data baru dari tipe data yang sudah ada
 * Type Declarations biasanya digunakan untuk membuat alias terhadap tipe data yang sudah ada, dengan tujuan agar lebih mudah dimengerti
-
 todo: end
 */
 
